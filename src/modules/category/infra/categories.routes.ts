e496ce5ab71238d24ede6e8ffc60dd47f9ec30ef
joaogf <joao.g.f@icloud.com>
2022-06-29 15:25:57 -0300

    upload csv file

--- src/modules/category/infra/categories.routes.ts
+++ src/modules/category/infra/categories.routes.ts
@@ -1,16 +1,30 @@
 import { Router } from 'express';
+import multer from 'multer';
+import { resolve } from 'node:path';
 
 import { CreateCategoryController } from '../useCases/CreateCategory/CreateCategoryController';
 import { DeleteCategoryController } from '../useCases/DeleteCategory/DeleteCategoryController';
 import { FindAllCategoriesController } from '../useCases/FindAllCategories/FindAllCategoriesController';
+import { ImportCategoriesController } from '../useCases/ImportCategories/ImportCategoriesController';
 
 export const categoriesRoutes = Router();
 
+const upload = multer({
+  dest: resolve(__dirname, '..', '..', '..', '..', 'tmp'),
+});
+
 const createCategoryController = new CreateCategoryController();
+const importCategoriesController = new ImportCategoriesController();
 const findAllCategoriesController = new FindAllCategoriesController();
 const deleteCategoryController = new DeleteCategoryController();
 
 categoriesRoutes.post('/', createCategoryController.handle);
+
+categoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  importCategoriesController.handle,
+);
 
 categoriesRoutes.get('/', findAllCategoriesController.handle);
 
---
+++ src/modules/category/useCases/ImportCategories/ImportCategoriesController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ImportCategoriesUseCase } from './ImportCategoriesUseCase';
+
+export class ImportCategoriesController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { file } = request;
+
+    const importCategoriesUseCase = container.resolve(ImportCategoriesUseCase);
+
+    const metrics = await importCategoriesUseCase.execute(file);
+
+    return response.json({ metrics });
+  }
+}
